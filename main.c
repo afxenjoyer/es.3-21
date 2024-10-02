237fb3b4e32778da50513fb1e4a67c3600565d22
Andrea Di Mingo <dimingo.andrea25@gmail.com>
2024-10-02 19:36:05 +0200

    Scritto il programma per l'esercizio 3.21

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int giorno;
+    int mese;
+    int anno;
+
+    printf("Inserisci il giorno della data da verificare\n");
+    scanf("%d",&giorno);
+    printf("Inserisci il mese della data da verificare\n");
+    scanf("%d",&mese);
+    printf("Inserisci l'anno della data da verificare\n");
+    scanf("%d",&anno);
+
+    if (mese >= 1 && mese <= 12 && giorno < 32)
+    {
+        if (mese == 2)
+        {
+            if (giorno > 28)
+            {
+                printf("La data digitata è sbagliata\n");
+            }
+            // (anno%4 == 0) check per controllare se l'anno è bisestile
+            else if (giorno > 29 && anno%4 == 0)
+            {
+                printf("La data digitata è sbagliata\n");
+            }
+            else
+            {
+                printf("La data digitata è corretta\n");
+            }
+        }
+        else if (mese == 4 || mese == 6 || mese == 9 || mese == 11)
+        {
+            if (giorno > 30)
+            {
+                printf("La data digitata è sbagliata\n");
+            }
+            else
+            {
+                printf("La data digitata è corretta\n");
+            }
+        }
+    }
+    else
+    {
+        printf("La data digitata è sbagliata\n");
+    }
+    return 0;
+}
